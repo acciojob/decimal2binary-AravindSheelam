5c5c69d7e4df76bdcd53ad5bfd8054e431a82f30
Sheelam Aravind <102232179+AravindSheelam@users.noreply.github.com>
2023-07-20 21:19:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+  // Write your code here
+	let arr=[];
+	let r;
+	let i=0;
+	while(num!=0){
+		r=num%2;//0 1 0 1
+		arr[i]+=r;//0101 
+		num=num/2;//0
+		i++;
+	}
+	arr.reverse();
+	return arr;
 }
 
 window.decimalToBinary = decimalToBinary;
